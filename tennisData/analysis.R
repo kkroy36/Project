c210a570129a797a18cb422996317936ff8a1bca
kkroy36 <kkroy36@users.noreply.github.com>
2016-03-25 01:16:49 -0400

    Update analysis.R

--- tennisData/analysis.R
+++ tennisData/analysis.R
@@ -9,4 +9,5 @@ y <- as.matrix(djok[1:18])%*%as.matrix(model$coefficients[2:19]) #y <- w*x
 y <- y + model$coefficients[1] #subtract intercept b i.e. y <- w*x + b
 sigma <- function(x) return(1/(1+exp(-x))) #calculate probability of y as sigma(y)
 p <- sigma(y) #this is the probability of djokovic winning given the training data
-accuracy <- (1 - sum(abs(p - djok$Result))/length(djok$Result)) * 100
+chat <- as.numeric(p > 0.5)
+accuracy <- (1 - sum(abs(chat - djok$Result))/length(djok$Result)) * 100
