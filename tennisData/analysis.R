1cd7ca3fba171267cba94029e366c19993409ec6
kkroy36 <kkroy36@users.noreply.github.com>
2016-03-25 01:12:20 -0400

    Update analysis.R

--- tennisData/analysis.R
+++ tennisData/analysis.R
@@ -5,7 +5,8 @@ djok <- djok[,c(32:50)]
 #given players stat what is the probability of winning against djokovic??
 #perform logisitc regression
 model <- glm(Result ~.,family=binomial(link='logit'),data=djok)
-y <- as.matrix(djok[1:18])%*%as.matrix(model$coefficients[2:19])
-y <- y - 0.59037852
-sigma <- function(x) return(1/(1+exp(-x)))
-p <- sigma(y)
+y <- as.matrix(djok[1:18])%*%as.matrix(model$coefficients[2:19]) #y <- w*x
+y <- y + model$coefficients[1] #subtract intercept b i.e. y <- w*x + b
+sigma <- function(x) return(1/(1+exp(-x))) #calculate probability of y as sigma(y)
+p <- sigma(y) #this is the probability of djokovic winning given the training data
+accuracy <- (1 - sum(abs(p - djok$Result))/length(djok$Result)) * 100
