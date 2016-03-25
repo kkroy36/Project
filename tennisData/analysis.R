b5d655f02ea47538f8bd6c2104024bbbce067252
Shyamal Roy <Kaushik@149-160-156-19.dhcp-bl.indiana.edu>
2016-03-25 01:08:56 -0400

    regression update

--- tennisData/analysis.R
+++ tennisData/analysis.R
@@ -1,3 +1,11 @@
-rm(list=ls()) #clear workspace
-data <- na.omit(read.csv("data.csv")) #read data
-data[,1] <- NULL #remove false ordering
+rm(list=ls())
+source("read.R")
+djok <- djok[,c(32:50)]
+
+#given players stat what is the probability of winning against djokovic??
+#perform logisitc regression
+model <- glm(Result ~.,family=binomial(link='logit'),data=djok)
+y <- as.matrix(djok[1:18])%*%as.matrix(model$coefficients[2:19])
+y <- y - 0.59037852
+sigma <- function(x) return(1/(1+exp(-x)))
+p <- sigma(y)
