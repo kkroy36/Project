df8de7e94999b6dfc9a81083fe4f43f02f2ec9ee
Shyamal Roy <Kaushik@149-160-156-19.dhcp-bl.indiana.edu>
2016-03-30 12:31:36 -0400

    threshold update

--- tennisData/analysis.R
+++ tennisData/analysis.R
@@ -1,13 +1,13 @@
 rm(list=ls())
 source("read.R")
 djok <- djok[,c(32:50)]
-
 #given players stat what is the probability of winning against djokovic??
 #perform logisitc regression
 model <- glm(Result ~.,family=binomial(link='logit'),data=djok)
-y <- as.matrix(djok[1:18])%*%as.matrix(model$coefficients[2:19]) #y <- w*x
+l <- length(model$coefficients)
+y <- as.matrix(djok[,1:(l-1)])%*%as.matrix(model$coefficients[2:l]) #y <- w*x
 y <- y + model$coefficients[1] #subtract intercept b i.e. y <- w*x + b
 sigma <- function(x) return(1/(1+exp(-x))) #calculate probability of y as sigma(y)
 p <- sigma(y) #this is the probability of djokovic winning given the training data
-chat <- as.numeric(p > 0.5)
-accuracy <- (1 - sum(abs(chat - djok$Result))/length(djok$Result)) * 100
+chat <- as.numeric(p > 0.52) #0.52 chosen as best by checking in a loop
+accuracy <- (1 - sum(abs(chat - djok$Result))/length(djok$Result)) * 100 #accuracy 81.78914%
